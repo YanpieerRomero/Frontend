0f1a587176a92cc3ae321b15bc624b2e2b359484
Yanpieer <yanpieer.romero@scanntech.com>
2023-02-23 18:45:21 -0500

    Delete unuse comments and imports

--- Angular/app-short-url/src/app/interceptors/short.interceptor.ts
+++ Angular/app-short-url/src/app/interceptors/short.interceptor.ts
@@ -12,7 +12,7 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class ShortInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { /* TODO document why this constructor is empty */ }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const TOKEN = 'api-key-bitly';
--- Angular/app-short-url/src/app/services/short-url.service.ts
+++ Angular/app-short-url/src/app/services/short-url.service.ts
@@ -1,28 +1,23 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShortUrlService {
   url: string;
-  //token: string;
 
   constructor(private http: HttpClient) {
     this.url = 'https://api-ssl.bitly.com/v4/shorten';
-    //this.token = 'api-key-bitly';
   }
 
   getUrlShort(urlName: string): Observable<any> {
-    //const tokenHeader = new HttpHeaders({Authorization: 'Bearer ' + this.token});
-
     const body = {
       long_url: urlName,
     };
 
-    return this.http.post(this.url, body, /* { headers: tokenHeader} */)
+    return this.http.post(this.url, body)
   }
   
 }
